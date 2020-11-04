1fc864ffcf48a3a910e4932a550b97ec7cf2c5c5
gituserraju <git.user.raju@gmail.com>
2020-11-04 19:16:46 +0530

    1st

---
+++ src/redux/user/userActions.js
@@ -0,0 +1,22 @@
+import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from "./userTypes"
+
+
+export const fetchusersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUEST
+    }
+}
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+
+const fetchUsersFailure = error => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+}
